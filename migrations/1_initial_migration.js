bd3d9e08a84a4a9064586c1433014f32fbc3619a
Merunas Grincalaitis <merunasgrincalaitis@gmail.com>
2020-09-12 00:43:11 +0200

    Working on adding oracle

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,16 @@
-const Migrations = artifacts.require("Migrations");
+// yVault
+// Controller
+// Strategy
+// Governance Proxy (? Maybe its a multisignature wallet
+// TreasuryVault
+
+const yVault = artifacts.require('yVault')
+const Controller = artifacts.require('Controller')
+const StrategyDAICurve = artifacts.require('StrategyDAICurve')
+const Governance = artifacts.require()
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(yVault).then(yVault => {
+    
+  })
 };
